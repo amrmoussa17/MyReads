fa1d40a1acdcee50ecc9cebbb896abdee11ed8fb
amrmoussa17 <amrmamdoh@hotmail.com>
2022-06-20 16:01:10 +0200

    refactored code and handled books api at first render

--- starter/src/BookShelf.js
+++ starter/src/BookShelf.js
@@ -15,7 +15,7 @@ function BookShelf(props) {
                       style={{
                         width: 128,
                         height: 193,
-                        backgroundImage: `url(${book.imageLink})`,
+                        backgroundImage: `url(${book.imageLinks.thumbnail})`,
                       }}
                     ></div>
                     <div className="book-shelf-changer">
